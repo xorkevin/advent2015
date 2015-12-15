400e1fa0c2583b31797ded5a8ea125b2a3ffbb3c
Kevin Wang <wangkevin448@gmail.com>
2015-12-15 00:42:44 -0800

    finish parsing advent 9

--- advent9.py
+++ advent9.py
@@ -1,3 +1,5 @@
+from itertools import permutations
+
 class Node:
     def __init__(self, key):
         '''paths is a dictionary that has key - node and value - cost of path to node'''
@@ -7,6 +9,9 @@ class Node:
     def connect(self, other, cost):
         '''other should be a key of another node; cost is to that node from this'''
         self._paths[other] = cost
+
+    def getCost(self, other):
+        return self._paths[other]
 
 
 class Network:
@@ -21,7 +26,18 @@ class Network:
 
     def getNode(self, name):
         return self._nodedict[name]
-        
+
+    def getCost(self, path):
+        '''path should be a list of node keys'''
+        if len(path) < 2:
+            return 0
+
+        cost = 0
+        for i in range(1, len(path)):
+            cost += self.getNode(path[i-1]).getCost(path[i])
+
+        return cost
+
 
 def travelSalesman(graph):
     '''this finds the shortest tour through each node of a network
@@ -35,4 +51,29 @@ def minSpanTree(graph):
 
 def bruteForceSalesman(graph):
     '''checks all possible permutations'''
-    pass
+    longest = 0
+    longestPath = None
+    shortest = sys.maxsize
+    shortestPath = None
+
+    for i in permutations(list(graph._nodes)):
+        pathCost = graph.getCost(i)
+        if longest < pathCost:
+            longest = pathCost
+            longestPath = i
+        if shortest > pathCost:
+            shortest = pathCost
+            shortestPath = i
+
+    return {'longest': {'cost': longest, 'path': longestPath}, 'shortest': {'cost': shortest, 'path': shortestPath}}
+
+def parseInstruct(instructs, network):
+    for i in instructs:
+        pass
+        #need to check for uniqueness of nodes, do not overwrite same node
+
+f = None
+with open('advent9data.txt') as d:
+    f = d.read().strip().split('\n')
+
+net = Network()
